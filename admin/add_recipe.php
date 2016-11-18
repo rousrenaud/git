de2f2cec2674012cb2e036a5dd4dd3592a18ecd4
AnthonyDoux <anthony.doux@live.fr>
2016-11-18 16:47:15 +0100

    update

--- admin/add_recipe.php
+++ admin/add_recipe.php
@@ -14,87 +14,83 @@ $dirUpload ='uploads/';
 // Vérification des champs et déclaration des erreurs
 
 if(!empty($_POST) && $is_logged){
-	// nettoyage des données entrées
-	$post = array_map('trim', array_map('strip_tags', $_POST));
-
-	if(!minAndMaxLength($post['recipe_author'], 5, 50)){
-		$errors[] = 'Le nom de votre recette doit comprendre entre 5 et 50 caractères';
-	}
+    // nettoyage des données entrées
+    $post = array_map('trim', array_map('strip_tags', $_POST));
     
-    if(!minAndMaxLength($post['recipe_title'], 5, 50)){
-		$errors[] = 'Le nom de votre recette doit comprendre entre 5 et 50 caractères';
-	}
+    if(!preg_match('/[(\w+\s)]{5,140}/i',$post['recipe_title'])){
+        $errors[] = 'Le nom de votre recette doit comprendre entre 5 et 
+        140 caractères';
+    }
 
-	if(!is_numeric($post['recipe_time']) || empty($post['recipe_time'])) {
-		$errors[] = 'Le temps de préparation indiqué est incorrect';
-	}
+    if(!is_numeric($post['recipe_time']) || empty($post['recipe_time'])) {
+        $errors[] = 'Le temps de préparation indiqué est incorrect';
+    }
 
-	if(!is_numeric($post['cook_time']) || empty($post['cook_time'])) {
-		$errors[] = 'Le temps de cuisson indiqué est incorrect';
-	}	
+    if(!is_numeric($post['cook_time']) || empty($post['cook_time'])) {
+        $errors[] = 'Le temps de cuisson indiqué est incorrect';
+    }   
 
-	if(!is_numeric($post['people'])) {
-		$errors[] = 'Le nombre de personnes indiqué est invalide';
-	}
+    if(!is_numeric($post['people'])) {
+        $errors[] = 'Le nombre de personnes indiqué est invalide';
+    }
 
-	if(!minAndMaxLength($post['ingredients'], 5, 5000)) {
-		$errors[] = 'Votre liste d\'ingrédients doit comprendre entre 5 et 5 000 caractères'; 
-	}
+    if(!minAndMaxLength($post['ingredients'], 5, 5000)) {
+        $errors[] = 'Votre liste d\'ingrédients doit comprendre entre 5 et 5 000 caractères'; 
+    }
 
-	if(!minAndMaxLength($post['preparation'], 5, 10000)) {
-		$errors[] = 'Votre liste d\'ingrédients doit comprendre entre 5 et 10 000 caractères'; 
-	}
+    if(!preg_match('/[(\w+\s)]{5,140}/i',$post['preparation'])) {
+        $errors[] = 'Votre recette doit comprendre entre 5 et 10 000 caractères'; 
+    }
 
-	if(!minAndMaxLength($post['advice'], 5, 500)) {
-		$errors = 'Les conseils doivent comprendre entre 5 et 500 caractères';
-	}
+    if(!minAndMaxLength($post['advice'], 5, 500)) {
+        $errors = 'Les conseils doivent comprendre entre 5 et 500 caractères';
+    }
 
-	// vérification de l'upload de fichier et envoi au serveur 
-	if(!is_uploaded_file($_FILES['photo']['tmp_name']) || !file_exists($_FILES['photo']['tmp_name'])){
-		$errors[] = 'Vous devez ajouter une photo de votre recette';
-	}
-	else{
-		$finfo = new finfo();
-		$mimeType = $finfo->file($_FILES['photo']['tmp_name'], FILEINFO_MIME_TYPE);
+    // vérification de l'upload de fichier et envoi au serveur 
+    if(!is_uploaded_file($_FILES['photo']['tmp_name']) || !file_exists($_FILES['photo']['tmp_name'])){
+        $errors[] = 'Vous devez ajouter une photo de votre recette';
+    }
+    else{
+        $finfo = new finfo();
+        $mimeType = $finfo->file($_FILES['photo']['tmp_name'], FILEINFO_MIME_TYPE);
 
-		if(in_array($mimeType, $mimeTypeAllow)){
-			$photoName = uniqid('photo_');
-			$photoName.= '.'.pathinfo($_FILES['photo']['name'], PATHINFO_EXTENSION);
+        if(in_array($mimeType, $mimeTypeAllow)){
+            $photoName = uniqid('photo_');
+            $photoName.= '.'.pathinfo($_FILES['photo']['name'], PATHINFO_EXTENSION);
 
-			if(!is_dir($dirUpload)){
-				mkdir($dirUpload, 0755);
-			}
+            if(!is_dir($dirUpload)){
+                mkdir($dirUpload, 0755);
+            }
 
-			if(!move_uploaded_file($_FILES['photo']['tmp_name'], $dirUpload.$photoName)) {
-				$errors[] = 'Erreur lors de l\'envoi de votre photo';
-			}
-		}
-		else {
-			$errors[] = 'Le type de fichier est invalide. Uniquement jpg/jpeg/png.';
-		}
-	}
-	// Vérification des erreurs et envoi en DB
-	if(count($errors) === 0){
-		$insert = $bdd->prepare('INSERT INTO recipes(id_user, recipe_author, recipe_title, recipe_time, cook_time, people, ingredients, preparation, advice, photo, date_publish) VALUES(:id_user, :recipe_author, :recipe_title, :recipe_time, :cook_time, :people, :ingredients, :preparation, :advice, :photo, NOW())');
+            if(!move_uploaded_file($_FILES['photo']['tmp_name'], $dirUpload.$photoName)) {
+                $errors[] = 'Erreur lors de l\'envoi de votre photo';
+            }
+        }
+        else {
+            $errors[] = 'Le type de fichier est invalide. Uniquement jpg/jpeg/png.';
+        }
+    }
+    // Vérification des erreurs et envoi en DB
+    if(count($errors) === 0){
+        $insert = $bdd->prepare('INSERT INTO recipes(id_user, recipe_title, recipe_time, cook_time, people, ingredients, preparation, advice, photo, date_publish) VALUES(:id_user, :recipe_title, :recipe_time, :cook_time, :people, :ingredients, :preparation, :advice, :photo, NOW())');
         
         $insert->bindValue(':id_user', $_SESSION['id']);
-        $insert->bindValue(':recipe_author', $post['recipe_author']);
-		$insert->bindValue(':recipe_title', $post['recipe_title']);
-		$insert->bindValue(':recipe_time', $post['recipe_time']);
-		$insert->bindValue(':cook_time', $post['cook_time']);
-		$insert->bindValue(':people', $post['people']);
-		$insert->bindValue(':ingredients', $post['ingredients']);
-		$insert->bindValue(':preparation', $post['preparation']);
-		$insert->bindValue(':advice', $post['advice']);
-		$insert->bindValue(':photo', $dirUpload.$photoName);
+        $insert->bindValue(':recipe_title', $post['recipe_title']);
+        $insert->bindValue(':recipe_time', $post['recipe_time']);
+        $insert->bindValue(':cook_time', $post['cook_time']);
+        $insert->bindValue(':people', $post['people']);
+        $insert->bindValue(':ingredients', $post['ingredients']);
+        $insert->bindValue(':preparation', $post['preparation']);
+        $insert->bindValue(':advice', $post['advice']);
+        $insert->bindValue(':photo', $dirUpload.$photoName);
 
-		if($insert->execute()){
-			$formValid = true;
-		}
-		else {
-			var_dump($insert->errorInfo());
-		}
-	}
+        if($insert->execute()){
+            $formValid = true;
+        }
+        else {
+            var_dump($insert->errorInfo());
+        }
+    }
 }
 
 
@@ -107,9 +103,9 @@ if(!empty($_POST) && $is_logged){
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
-	<title>Ajouter une recette</title>
-	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <meta charset="utf-8">
+    <title>Ajouter une recette</title>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
     <link rel="stylesheet" href="style/main.css">
 </head>
@@ -134,16 +130,9 @@ if(!empty($_POST) && $is_logged){
 
             <form method="post" class="form-horizontal" enctype="multipart/form-data">
 
-                <!-- Auteur -->
-                <div class="form-group">
-                    <label class="col-md-4 control-label" for="recipe_author">Auteur de la recette : </label>
-                    <div class="col-md-6">
-                        <input type="text" name="recipe_author" id="recipe_author" class="form-control input-md" placeholder="ex: Maïté...">
-                    </div>
-                </div>
                 
                 <!-- Nom de la recette -->
-                <div class="form-group">	
+                <div class="form-group">    
                     <label class="col-md-4 control-label" for="recipe_title">Nom de la recette : </label>
                     <div class="col-md-6">
                         <input type="text" name="recipe_title" id="recipe_title" class="form-control input-md" placeholder="ex: Magrets de canard au miel...">
@@ -210,7 +199,7 @@ if(!empty($_POST) && $is_logged){
                     </div>
                 </div>
 
-                <!-- Avatar --> 
+                <!-- Photo recette --> 
                 <div class="form-group">    
                     <label class="col-md-4 control-label" for="photo">Photo de votre recette : </label>
                     <div class="col-md-6">
